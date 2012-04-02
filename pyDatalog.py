ac74334cdd640e9121c22ca52ed0d398cca0c79e
pcarbonn <pcarbonn@users.noreply.github.com>
2012-04-02 22:09:47 +0200

    add copyright notice, licence, before making project public

--- pyDatalog.py
+++ pyDatalog.py
@@ -1,9 +1,27 @@
 """
+pyDatalog
+
+Copyright (C) 2012 Pierre Carbonnelle
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc.  51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+USA
+
+
 TODO:
-* put in bitbucket, using mercurialEclipse
 * next + odd/even --> performance ?
 * improve API
-* choose a licence
 * expression in == operator see __iadd__ and __radd__
 * test factorial
 * package for release on pyPi
