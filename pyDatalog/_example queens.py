e0c2e58029909c2306ce34140e38e21197336f1c
pcarbonn <pcarbonn@users.noreply.github.com>
2012-07-26 17:12:11 +0200

    improve n_queens speed through more memoization

--- pyDatalog/_example queens.py
+++ pyDatalog/_example queens.py
@@ -2,34 +2,40 @@
 import pyDatalog # or from pyDatalog import pyDatalog
 import time
 
+# with more memoization
+
 pyDatalog.clear()
 @pyDatalog.program()
 def _():
-    # when is it ok to have a queen in row X1 and another in row X2, separated by N columns
-    ok(X1, X2, N) <= (X1!=X2) & (X1!= X2+N) & (X1!=X2-N)
-    
     queens0(X0) <= (X0 in range(8))
-    queens1(X0,X1) <= (queens0(X0) & (X1 in range(8)) 
-        & ok(X1,X0,1))
-    queens2(X0,X1,X2) <= (queens1(X0,X1) & (X2 in range(8)) 
-        & ok(X2,X1,1) & ok(X2,X0,2))
-    queens3(X0,X1,X2,X3) <= (queens2(X0,X1,X2) & (X3 in range(8)) 
-        & ok(X3,X2,1) & ok(X3,X1,2) & ok(X3,X0,3))
-    queens4(X0,X1,X2,X3,X4) <= (queens3(X0,X1,X2,X3) & (X4 in range(8)) 
-        & ok(X4,X3,1) & ok(X4,X2,2) & ok(X4,X1,3) & ok(X4,X0,4))
-    queens5(X0,X1,X2,X3,X4,X5) <= (queens4(X0,X1,X2,X3,X4) & (X5 in range(8)) 
-        & ok(X5,X4,1) & ok(X5,X3,2) & ok(X5,X2,3) & ok(X5,X1,4) & ok(X5,X0,5))
-    queens6(X0,X1,X2,X3,X4,X5,X6) <= (queens5(X0,X1,X2,X3,X4,X5) & (X6 in range(8)) 
-        & ok(X6,X5,1) & ok(X6,X4,2) & ok(X6,X3,3) & ok(X6,X2,4) & ok(X6,X1,5) & ok(X6,X0,6))
-    queens7(X0,X1,X2,X3,X4,X5,X6,X7) <= (queens6(X0,X1,X2,X3,X4,X5,X6) & (X7 in range(8)) 
-        & ok(X7,X6,1) & ok(X7,X5,2) & ok(X7,X4,3) & ok(X7,X3,4) & ok(X7,X2,5) & ok(X7,X1,6) & ok(X7,X0,7))
+
+    # when is it ok to have a queen in row X1 and another in row X2, separated by N columns
+    # this is memoized !
+    ok(X1, N, X2) <= (X1!=X2) & (X1!= X2+N) & (X1!=X2-N)
+    
+    queens1(X0,X1) <= queens0(X0) & (X1 in range(8)) & ok(X1,1,X0)
+
+    queens2(X0,X1,X2) <= queens1(X0,X1) & (X2 in range(8)) & ok(X2, 1, X1, X0)
+    # when is it ok to have 2 queens N columns away from another one ?  Also memoized
+    ok(X2, N, X1, X0) <= ok(X2,N,X1) & (N1==N+1) & ok(X2,N1,X0)
+
+    queens3(X0,X1,X2,X3) <= queens2(X0,X1,X2) & (X3 in range(8))  & ok(X3, 1, X2, X1, X0)
+    ok(X3, N, X2, X1, X0) <= ok(X3,N, X2,X1)& (N1==N+2) & ok(X3,N1,X0)
+
+    queens4(X0,X1,X2,X3,X4) <= queens3(X0,X1,X2,X3) & (X4 in range(8)) & ok(X4,1, X3, X2, X1) & ok(X4,4,X0)
+
+    queens5(X0,X1,X2,X3,X4,X5) <= queens4(X0,X1,X2,X3,X4) & (X5 in range(8)) & ok(X5,1,X4,X3,X2) & ok(X5,4,X1,X0)
+
+    queens6(X0,X1,X2,X3,X4,X5,X6) <= queens5(X0,X1,X2,X3,X4,X5) & (X6 in range(8)) & ok(X6,1,X5,X4,X3) & ok(X6,4,X2,X1,X0)
+    # counting is 0-based, so this is actually the 8-queens solution
+    queens7(X0,X1,X2,X3,X4,X5,X6,X7) <= queens6(X0,X1,X2,X3,X4,X5,X6) & (X7 in range(8)) & ok(X7,1,X6,X5,X4) & ok(X7,4,X3,X2,X1) & ok(X7,7,X0)
 
 # counting is 0-based, so this is actually the 8-queens solution
 print(pyDatalog.ask("queens7(X0,X1,X2,X3,X4,X5,X6,X7)"))
     
 # there is a fixed penalty the first time around (JIT, ...), so let's measure performance the second time
 start_time = time.time()
-print(pyDatalog.ask("queens7(X0,X1,X2,X3,X4,X5,X6,X7)"))
+datalog_count = len(pyDatalog.ask("queens7(X0,X1,X2,X3,X4,X5,X6,X7)").answers)
 datalog_time = (time.time() - start_time)
 
 # pure python solution found on http://rosettacode.org/wiki/N-Queens#Python, for comparison purposes
@@ -47,5 +53,5 @@ for vec in permutations(cols):
         pass
 python_time = time.time() - start_time
 
-print("datalog : %f seconds" % datalog_time)
+print("%i solutions by datalog in %f seconds" % (datalog_count, datalog_time))
 print("python : %f seconds" % python_time)
