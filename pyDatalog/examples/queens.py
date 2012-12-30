3f658e60294df06481b28d808226bea1981c24aa
pcarbonn <pcarbonn@localhost>
2012-12-30 18:29:15 +0100

    use create_atoms in queens.py example. Fix issue with stringification of in-line comparison. add __init__.py to examples directory.

--- pyDatalog/examples/queens.py
+++ pyDatalog/examples/queens.py
@@ -2,28 +2,28 @@
 from pyDatalog import pyDatalog
 import time
 
-pyDatalog.clear()
-@pyDatalog.program()
-def _():
-    queens0(X0) <= (X0 in range(8))
+pyDatalog.create_atoms('N,X0,X1,X2,X3,X4,X5,X6,X7')
+pyDatalog.create_atoms('ok,queens0,queens1,queens2,queens3,queens4,queens5,queens6,queens7')
 
-    # when is it ok to have a queen in row X1 and another in row X2, separated by N columns
-    # this is memoized !
-    ok(X1, N, X2) <= (X1!=X2) & (X1!= X2+N) & (X1!=X2-N)
-    
-    queens1(X0,X1) <= queens0(X0) & queens0(X1) & ok(X1,1,X0)
+queens0(X0) <= (X0._in(range(8)))
 
-    queens2(X0,X1,X2) <= queens1(X0,X1) & queens1(X1,X2) & ok(X0,2,X2)
+# when is it ok to have a queen in row X1 and another in row X2, separated by N columns
+# this is memoized !
+ok(X1, N, X2) <= (X1!=X2) & (X1!= X2+N) & (X1!=X2-N)
 
-    queens3(X0,X1,X2,X3) <= queens2(X0,X1,X2) & queens2(X1,X2,X3) & ok(X0,3,X3)
+queens1(X0,X1) <= queens0(X0) & queens0(X1) & ok(X1,1,X0)
 
-    queens4(X0,X1,X2,X3,X4) <= queens3(X0,X1,X2,X3) & queens3(X1,X2,X3,X4) & ok(X0,4,X4)
+queens2(X0,X1,X2) <= queens1(X0,X1) & queens1(X1,X2) & ok(X0,2,X2)
 
-    queens5(X0,X1,X2,X3,X4,X5) <= queens4(X0,X1,X2,X3,X4) & queens4(X1,X2,X3,X4,X5) & ok(X0,5,X5)
+queens3(X0,X1,X2,X3) <= queens2(X0,X1,X2) & queens2(X1,X2,X3) & ok(X0,3,X3)
 
-    queens6(X0,X1,X2,X3,X4,X5,X6) <= queens5(X0,X1,X2,X3,X4,X5) & queens5(X1,X2,X3,X4,X5,X6) & ok(X0,6,X6)
-    # counting is 0-based, so this is actually the 8-queens solution
-    queens7(X0,X1,X2,X3,X4,X5,X6,X7) <= queens6(X0,X1,X2,X3,X4,X5,X6) & queens6(X1,X2,X3,X4,X5,X6,X7) & ok(X0,7,X7)
+queens4(X0,X1,X2,X3,X4) <= queens3(X0,X1,X2,X3) & queens3(X1,X2,X3,X4) & ok(X0,4,X4)
+
+queens5(X0,X1,X2,X3,X4,X5) <= queens4(X0,X1,X2,X3,X4) & queens4(X1,X2,X3,X4,X5) & ok(X0,5,X5)
+
+queens6(X0,X1,X2,X3,X4,X5,X6) <= queens5(X0,X1,X2,X3,X4,X5) & queens5(X1,X2,X3,X4,X5,X6) & ok(X0,6,X6)
+# counting is 0-based, so this is actually the 8-queens solution
+queens7(X0,X1,X2,X3,X4,X5,X6,X7) <= queens6(X0,X1,X2,X3,X4,X5,X6) & queens6(X1,X2,X3,X4,X5,X6,X7) & ok(X0,7,X7)
 
 # counting is 0-based, so this is actually the 8-queens solution
 print(pyDatalog.ask("queens7(X0,X1,X2,X3,X4,X5,X6,X7)"))
