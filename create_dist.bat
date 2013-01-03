50221f13983bb212927f39ce7d52a53dbed18c2e
pcarbonn <pcarbonn@localhost>
2013-01-03 23:32:28 +0100

    update grammar documentation.  Remove windows distribution.

--- create_dist.bat
+++ create_dist.bat
@@ -2,5 +2,5 @@ cd "C:\Users\pcarbonn\Documents\98 Eclipse\pyDatalog\build\lib"
 del *.*
 cd "C:\Users\pcarbonn\Documents\98 Eclipse\pyDatalog"
 Setup.py sdist
-Setup.py bdist_wininst
+rem Setup.py bdist_wininst
 pause
