bcf34df80c9c18a9e8f91f97a561b3e011d00bfa
pcarbonn <pcarbonn@users.noreply.github.com>
2013-01-03 23:32:28 +0100

    update grammar documentation.  Remove windows distribution.

--- upload.bat
+++ upload.bat
@@ -3,5 +3,5 @@ pause
 cd "C:\Users\pcarbonn\Documents\98 Eclipse\pyDatalog\build\lib"
 del *.*
 cd "C:\Users\pcarbonn\Documents\98 Eclipse\pyDatalog"
-setup.py register sdist bdist_wininst upload
+setup.py register sdist upload
 pause
