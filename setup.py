9a599e55ed1511333db00502cf7ba63bd796acc7
pcarbonn <pcarbonn@localhost>
2013-01-03 18:45:41 +0100

    version 0.12.0

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         ],
     long_description = """\
-pyDatalog adds the logic programming paradigm to python's toolbox, in a pythonic way.  
-You can now run logic queries on databases or python objects, and use logic clauses to define python classes.
+pyDatalog adds the logic programming paradigm to Python's toolbox, in a pythonic way.  
+You can now run logic queries on databases or Python objects, and use logic clauses to define python classes.
 
 Datalog is a truly declarative subset of prolog that is best at
 
@@ -37,14 +37,14 @@ Datalog is a truly declarative subset of prolog that is best at
 * performing recursive algorithms (e.g. in network protocol, code and graph analysis)
 * solving discrete constraint problems. 
 
-In particular, pyDatalog can be used for object-relational mapping: 
+In particular, pyDatalog can be used as a query language: 
 
-* it can perform multi-database queries (from memory datastore,  relational databases, and noSQL database with appropriate connectors)
+* it can perform multi-database queries (from memory datastore, 11 relational databases, and noSQL database with appropriate connectors)
 * it is more expressive than SQL, with a cleaner syntax; 
 * it facilitates re-use of SQL code snippet (e.g. for frequent joins or formula); 
 * it offloads the database server by performing joins on the application tier. 
 
-Datalog excels at accelerated development : Datalog programs are often shorter than their python equivalent, 
+Datalog excels at accelerated development : Datalog programs are often shorter than their Python equivalent, 
 and Datalog statements can be specified in any order, as simply as formula in a spreadsheet.""",
     setup_requires = ["six"],
     install_requires = ["six"]
