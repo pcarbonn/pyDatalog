ec3995c3a20e4f6f5c18549571890715e02f404d
pcarbonn <pcarbonn@users.noreply.github.com>
2013-12-27 18:01:47 +0100

    add IPython notebook for tutorial

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name = "pyDatalog",
     packages = ["pyDatalog", "pyDatalog/examples"],
     version = version.__version__,
-    description = "A pure-python implementation of a truly declarative language derived from prolog.  Run logic queries on databases or python objects, and use logic clauses to define python classes.",
+    description = "A pure-python implementation of Datalog, a truly declarative language derived from Prolog.",
     author = "Pierre Carbonnelle",
     author_email = "pierre.carbonnelle@gmail.com",
     url = "https://sites.google.com/site/pydatalog/",
     download_url = "http://pypi.python.org/pypi?name=pyDatalog&:action=display",
-    keywords = "prolog, logic programming, datalog, object-relational mapping, data integration",
+    keywords = "prolog, logic programming, database, SQL, data integration, expert system, AI",
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
@@ -27,14 +27,15 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         ],
     long_description = """\
-pyDatalog adds the logic programming paradigm to Python's toolbox, in a pythonic way.  
-You can now run logic queries on databases or Python objects, and use logic clauses to define python classes.
+pyDatalog adds the logic Programming paradigm to Python's toolbox, in a pythonic way.
+
+Logic programmers can now use the extensive standard library of Python, and Python programmers can now express complex algorithms simply.  
 
 Datalog is a truly declarative language derived from Prolog, with strong academic foundations.  It complements Python very well for:
 
 * managing complex sets of related information (e.g. in data integration or the semantic web).  
-* simulating intelligent behavior (e.g. in games), 
-* performing recursive algorithms (e.g. in network protocol, code and graph analysis, parsing)
+* simulating intelligent behavior (e.g. in games or expert systems), 
+* performing recursive algorithms (e.g. in network protocol, code and graph analysis)
 * solving discrete constraint problems. 
 
 In particular, pyDatalog can be used as a query language: 
@@ -43,6 +44,6 @@ In particular, pyDatalog can be used as a query language:
 * it is more expressive than SQL, with a cleaner syntax; 
 * it facilitates re-use of SQL code snippet (e.g. for frequent joins or formula); 
 
-Datalog excels at accelerated development : Datalog programs are often shorter than their Python equivalent, 
-and Datalog statements can be specified in any order, as simply as formula in a spreadsheet."""
+Datalog excels at managing complexity.  Datalog programs are often shorter than their Python equivalent, 
+and Datalog statements can be specified in any order, as simply as formula in a spreadsheet. """
 )
