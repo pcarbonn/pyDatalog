61bae640047eb1bc073687f28a67e143415e14a4
pcarbonn <pcarbonn@localhost>
2014-02-20 23:29:34 +0100

    support wheel distribution

--- create_dist.bat
+++ create_dist.bat
@@ -1,6 +1,7 @@
 cd "C:\Users\pcarbonn\Documents\98 Eclipse\pyDatalog\build\lib"
 del *.*
 cd "C:\Users\pcarbonn\Documents\98 Eclipse\pyDatalog"
-Setup.py sdist
+c:\python27\python.exe Setup.py sdist
+c:\python27\python.exe setup.py bdist_wheel upload
 rem Setup.py bdist_wininst
 pause
--- setup.py
+++ setup.py
@@ -31,19 +31,19 @@ pyDatalog adds the logic Programming paradigm to Python's toolbox, in a pythonic
 
 Logic programmers can now use the extensive standard library of Python, and Python programmers can now express complex algorithms simply.  
 
-Datalog is a truly declarative language derived from Prolog, with strong academic foundations.  It complements Python very well for:
+Datalog is a truly declarative language derived from Prolog, with strong academic foundations.  Datalog excels at managing complexity.  
+Datalog programs are shorter than their Python equivalent, and Datalog statements can be specified in any order, as simply as formula in a spreadsheet. 
 
-* managing complex sets of related information (e.g. in data integration or the semantic web).  
+In particular, Datalog can be used for:
+
 * simulating intelligent behavior (e.g. in games or expert systems), 
-* performing recursive algorithms (e.g. in network protocol, code and graph analysis)
+* performing recursive algorithms (e.g. in network protocol, code and graph analysis),
+* managing complex sets of related information (e.g. in data integration or the semantic web), 
 * solving discrete constraint problems. 
 
-In particular, pyDatalog can be used as a query language: 
+.. pull-quote::
 
-* it can perform multi-database queries (from memory datastore, 11 relational databases, and noSQL database with appropriate connectors)
-* it is more expressive than SQL, with a cleaner syntax; 
-* it facilitates re-use of SQL code snippet (e.g. for frequent joins or formula); 
+    "Datalog is to Python what Python was to C, and what C was to Assembly."
 
-Datalog excels at managing complexity.  Datalog programs are often shorter than their Python equivalent, 
-and Datalog statements can be specified in any order, as simply as formula in a spreadsheet. """
+ """
 )
