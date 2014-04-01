4d4a50133dbe47d4f966b76031d64b936877f73a
pcarbonn <pcarbonn@users.noreply.github.com>
2014-04-01 19:08:04 +0200

    create separate Aggregate.py module

--- pyDatalog/pyParser.py
+++ pyDatalog/pyParser.py
@@ -146,6 +146,7 @@ class Expression(object):
     @classmethod
     def _pyD_for(cls, operand):
         """ factory that converts an operand to an Expression """
+        from .Aggregate import Aggregate
         if isinstance(operand, (Expression, Aggregate)):
             return operand
         if isinstance(operand, type(lambda: None)) and (operand.__name__ if PY3 else operand.func_name) == '<lambda>':
@@ -321,6 +322,7 @@ class Symbol(Variable):
 
     def __call__ (self, *args, **kwargs):
         """ called when evaluating p(args) """
+        from . import Aggregate
         if self._pyD_name == 'ask': # call ask() and return an answer
             if 1<len(args):
                 raise RuntimeError('Too many arguments for ask !')
@@ -329,30 +331,30 @@ class Symbol(Variable):
         # manage the aggregate functions
         elif self._pyD_name in ('_sum', 'sum_'):
             if isinstance(args[0], VarSymbol):
-                return Sum_aggregate(args[0], for_each=kwargs.get('for_each', kwargs.get('key', [])))
+                return Aggregate.Sum(args[0], for_each=kwargs.get('for_each', kwargs.get('key', [])))
             else:
                 return sum(args)
         elif self._pyD_name in ('concat', 'concat_'):
-            return Concat_aggregate(args[0], order_by=kwargs.get('order_by',kwargs.get('key', [])), sep=kwargs['sep'])
+            return Aggregate.Concat(args[0], order_by=kwargs.get('order_by',kwargs.get('key', [])), sep=kwargs['sep'])
         elif self._pyD_name in ('_min', 'min_'):
             if isinstance(args[0], VarSymbol):
-                return Min_aggregate(args[0], order_by=kwargs.get('order_by',kwargs.get('key', [])),)
+                return Aggregate.Min(args[0], order_by=kwargs.get('order_by',kwargs.get('key', [])),)
             else:
                 return min(args)
         elif self._pyD_name in ('_max', 'max_'):
             if isinstance(args[0], VarSymbol):
-                return Max_aggregate(args[0], order_by=kwargs.get('order_by',kwargs.get('key', [])),)
+                return Aggregate.Max(args[0], order_by=kwargs.get('order_by',kwargs.get('key', [])),)
             else:
                 return max(args)
         elif self._pyD_name in ('rank', 'rank_'):
-            return Rank_aggregate(None, group_by=kwargs.get('group_by', []), order_by=kwargs.get('order_by', []))
+            return Aggregate.Rank(None, group_by=kwargs.get('group_by', []), order_by=kwargs.get('order_by', []))
         elif self._pyD_name in ('running_sum', 'running_sum_'):
-            return Running_sum(args[0], group_by=kwargs.get('group_by', []), order_by=kwargs.get('order_by', []))
+            return Aggregate.Running_sum(args[0], group_by=kwargs.get('group_by', []), order_by=kwargs.get('order_by', []))
         elif self._pyD_name == 'tuple_':
-            return Tuple(args[0], order_by=kwargs.get('order_by', []))
+            return Aggregate.Tuple(args[0], order_by=kwargs.get('order_by', []))
         elif self._pyD_name in ('_len', 'len_'):
             if isinstance(args[0], VarSymbol):
-                return Len_aggregate(args[0])
+                return Aggregate.Len(args[0])
             else: 
                 return len(args[0]) 
         elif self._pyD_name == 'range_':
@@ -461,6 +463,7 @@ class Literal(object):
     operator '<=' means 'is true if', and creates a Clause
     """
     def __init__(self, predicate_name, args, kwargs={}, prearity=None, aggregate=None):
+        from .Aggregate import Aggregate
         t = sorted(kwargs.items())
         self.predicate_name = '_'.join([predicate_name]+[p[0] for p in t])
         self.args = list(args) + [p[1] for p in t]
@@ -506,6 +509,7 @@ class Literal(object):
     @classmethod
     def make_for_comparison(cls, self, operator, other):
         """ factory of Literal (or Body) for a comparison. """
+        from .Aggregate import Aggregate
         other = Expression._pyD_for(other)
         if isinstance(other, Function) and operator == '==':
             self, other = other, self
@@ -703,222 +707,6 @@ def add_clause(head,body):
     return result
 
 
-##################################### Aggregation #####################################
-    
-from itertools import groupby
-
-class Aggregate(object):
-    """ 
-    represents a generic aggregation_method(X, for_each=Y, order_by=Z, sep=sep)
-    e.g. 'sum(Y,key=Z)' in '(a[X]==sum(Y,key=Z))'
-    pyEngine calls sort_result(), key(), reset(), add() and fact() to compute the aggregate
-    """
-    counter = util.Counter()
-    
-    def __init__(self, Y=None, group_by=tuple(), for_each=tuple(), order_by=tuple(), sep=None):
-        # convert for_each=Z to for_each=(Z,)
-        self.Y = Y
-        self.group_by = (group_by,) if isinstance(group_by, Expression) else tuple(group_by)
-        self.for_each = (for_each,) if isinstance(for_each, Expression) else tuple(for_each)
-        self.order_by = (order_by,) if isinstance(order_by, Expression) else tuple(order_by)
-        
-        # try to recast expressions to variables
-        self.group_by = tuple([e.__dict__.get('_pyD_variable', e) for e in self.group_by]) 
-        self.for_each = tuple([e.__dict__.get('_pyD_variable', e) for e in self.for_each]) 
-        self.order_by = tuple([e.__dict__.get('_pyD_variable', e) for e in self.order_by])
-        
-        if not all([isinstance(e, VarSymbol) for e in self.group_by + self.for_each + self.order_by]):
-            raise util.DatalogError("Arguments of aggregate must be variable(s).", None, None)
-        
-        if sep and not isinstance(sep, util.string_types):
-            raise util.DatalogError("Separator in aggregation must be a string", None, None)
-        self.sep = sep
-        
-        # verify presence of keyword arguments
-        for kw in self.required_kw:
-            arg = getattr(self, kw)
-            if arg is None or (isinstance(arg, tuple) and arg == tuple()):
-                raise util.DatalogError("Error: argument missing in aggregate", None, None)
-        
-        # used to create literal.
-        self.args = ((Y,) if Y is not None else tuple()) + self.group_by + self.for_each + self.order_by + ((sep,) if sep is not None else tuple())
-        self.Y_arity = 1 if Y is not None else 0
-        self.sep_arity = 1 if sep is not None else 0
-        
-    @property
-    def arity(self):
-        """returns the arity of the aggregate function, not of the full predicate """
-        return len(self.args)
-
-    def make_literal_for(self, function, operator):
-        if operator != '==':
-            raise util.DatalogError("Aggregate operator can only be used with equality.", None, None)
-
-        name = function._pyD_name + operator
-        result = function._pyD_symbol
-        #TODO perf : do not add pre-term for non prefixed #prefixed
-        terms = [VarSymbol.make_for_prefix(function._pyD_name)] + list(function._pyD_keys) + [result]  #prefixed
-        self.index_first_arg = len(terms)-1 # position of the value to add
-        
-        # 1 create literal that can be queried
-        head = Literal.make(name, terms, {}, prearity=len(terms)-1)
-
-        # 2 create clause to resolve it
-        
-        terms[-1:-1] = self.args # insert the aggregate arguments before the result
-
-        # determine list of variables, without duplication
-        variables, new_terms = {}, [] 
-        for variable in terms:
-            if isinstance(variable, VarSymbol) and variable._pyD_name not in variables:
-                variables[variable._pyD_name] = len(new_terms)
-                new_terms.append(variable)
-                
-        new_name = name + '!' + str(Aggregate.counter.next())
-        body = Literal.make(new_name, new_terms, {}, aggregate=self) #pred
-        add_clause(head, body)
-                
-        self.index_value = variables[self.Y._pyD_name] if self.Y is not None else None
-        self.slice_for_each = [variables[variable._pyD_name] for variable in self.for_each]
-        self.reversed_order_by = [variables[variable._pyD_name] for variable in self.order_by][::-1]
-        self.reverse_order = [variable._pyD_negated for variable in new_terms[:-1]]
-        if isinstance(self, Rank_aggregate): # can't use required_kw because rank does not require group_by
-            self.slice_group_by = [variables[variable._pyD_name] for variable in self.group_by]
-        else:
-            self.slice_group_by = [variables[Expression._pyD_for(variable)._pyD_name] 
-                                   for variable in function._pyD_keys if isinstance(variable, VarSymbol)]
-        self.slice_to_variabilize = [variables[variable._pyD_name] for variable in function._pyD_keys 
-                                     if isinstance(variable, VarSymbol) 
-                                     and variables[variable._pyD_name] not in self.slice_group_by]
-        
-        # return a literal without the result
-        new_literal = Literal.make(new_name, new_terms[:-1], {})
-        return new_literal
-        
-    def fact_candidate(self, subgoal, row):
-        if row is not None:
-            class0 = subgoal.literal.pred._class()
-            row[self.index_first_arg:-1] = [""] * (len(row)-self.index_first_arg-1)
-            pyEngine.fact_candidate(subgoal, class0, row)
-
-    def complete(self, base_subgoal, subgoal):
-        """ calculate the aggregate after base facts have been found """
-        #TODO avoid intermediate result
-        
-        result = [ tuple(l.terms) for l in list(base_subgoal.facts.values())]
-        if result:
-            self.sort_result(result)
-            for _, v in groupby(result, self.key):
-                self.reset()
-                for r in v:
-                    row = self.add(r)
-                    self.fact_candidate(subgoal, row)
-                row = self.fact(r)
-                self.fact_candidate(subgoal, row)
-        
-    def sort_result(self, result):
-        """ sort result according to the aggregate argument """
-        # first sort per order_by, allowing for _pyD_negated
-        for i in self.reversed_order_by:
-            result.sort(key=lambda literal, i=i, self=self: literal[i].id,
-                reverse = self.reverse_order[i])
-        # then sort per group_by
-        result.sort(key=lambda literal, self=self: [id(literal[i]) for i in self.slice_group_by]) # faster than .id; ok for group_by
-        pass
-    
-    def key(self, result):
-        """ return the grouping key of a result """
-        return list(result[i] for i in self.slice_group_by)
-    
-    def reset(self):
-        """ by default, _value is 0 """
-        self._value = 0
-        
-    @property
-    def value(self):
-        """ by default, value is _value"""
-        return self._value
-    
-    def fact(self, row):
-        """ returns the terms of an aggregated fact"""
-        return list(row) + [self.value]
-       
-class Sum_aggregate(Aggregate):
-    """ represents sum_(Y, for_each=(Z,T))"""
-    required_kw = ('Y', 'for_each')
-
-    def add(self, row):
-        self._value += row[self.index_value].id
-        
-class Len_aggregate(Aggregate, Operation):
-    """ represents len_(X) : a simple or aggregate operation"""
-    required_kw = ('Y')
-
-    def __init__(self, Y):
-        Aggregate.__init__(self, Y)
-        Operation.__init__(self, None, '#', Y)
-        
-    def add(self, row):
-        self._value += 1
-
-class Tuple(Aggregate):
-    """ represents tuple_(X, order_by=(Y,)"""
-    required_kw = ('Y', 'order_by')
-        
-    def reset(self):
-        self._value = []
-        
-    def add(self, row):
-        self._value.append(row[self.index_value].id)
-        
-    @property
-    def value(self):
-        return tuple(self._value)
-    
-class Concat_aggregate(Tuple):
-    """ represents concat_(Y, order_by=(Z1,Z2), sep=sep)"""
-    required_kw = ('Y', 'order_by', 'sep')
-
-    @property
-    def value(self):
-        return self.sep.join(self._value)
-
-class Min_aggregate(Aggregate):
-    """ represents min_(Y, order_by=(Z,T))"""
-    required_kw = ('Y', 'order_by')
-
-    def reset(self):
-        self._value = None
-        
-    def add(self, row):
-        self._value = row[self.index_value].id if self._value is None else self._value
-
-class Max_aggregate(Min_aggregate):
-    """ represents max_(Y, order_by=(Z,T))"""
-    def __init__(self, *args, **kwargs):
-        Min_aggregate.__init__(self, *args, **kwargs)
-        for a in self.order_by:
-            a._pyD_negated = not(a._pyD_negated)
-
-class Rank_aggregate(Aggregate):
-    """ represents rank_(group_by=Z, order_by=T)"""
-    required_kw = ('order_by',)
-    
-    def add(self, row):
-        self._value += 1
-        return list(row) + [self._value-1]
-        
-    def fact(self, k):
-        return None
-
-class Running_sum(Rank_aggregate):
-    """ represents running_sum(Y, group_by=Z, order_by=T"""
-    required_kw = ('Y', 'group_by', 'order_by')
-    
-    def add(self, row):
-        self._value += row[self.index_value].id
-        return list(row) + [self._value]
-        
         
 """                             Parser methods                                                   """
 
