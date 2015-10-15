f1fe17b2ea09b5bc3158f4c7ac0cf1994711a926
pcarbonn <pierre.carbonnelle@gmail.com>
2015-10-15 21:36:08 +0200

    again, use @pyDatalog.program() for implicit term creation

--- pyDatalog/examples/graph.py
+++ pyDatalog/examples/graph.py
@@ -10,9 +10,6 @@ This file illustrates algorithms that can be used with graphs, trees, hierarchie
 
 from pyDatalog import pyDatalog
 
-pyDatalog.create_terms('link, can_reach, path, all_path, safe_path, path_with_cost, shortest_path')
-pyDatalog.create_terms('X, Y, Z, P, P2, C, C2')
-
 #        4
 #       /
 #  1 - 2 - 3
@@ -21,62 +18,64 @@ pyDatalog.create_terms('X, Y, Z, P, P2, C, C2')
 #     \ /
 #      6       8 - 9
 
-+link(1,2)
-+link(2,3)
-+link(2,4)
-+link(2,5)
-+link(5,6)
-+link(6,7)
-+link(7,2)
-+link(8,9)
+@pyDatalog.program()
+def _():
+	+link(1,2)
+	+link(2,3)
+	+link(2,4)
+	+link(2,5)
+	+link(5,6)
+	+link(6,7)
+	+link(7,2)
+	+link(8,9)
 
-link(X,Y) <= link(Y,X) # optional : make each link bi-directional
+	link(X,Y) <= link(Y,X) # optional : make each link bi-directional
 
-print (link(1,Y))
+	print (link(1,Y))
 
-print("can reach from 1")
-can_reach(X,Y) <= can_reach(X,Z) & link(Z,Y) & (X!=Y)
-can_reach(X,Y) <= link(X,Y)
-print (can_reach(1,Y))
+	print("can reach from 1")
+	can_reach(X,Y) <= can_reach(X,Z) & link(Z,Y) & (X!=Y)
+	can_reach(X,Y) <= link(X,Y)
+	print (can_reach(1,Y))
 
-print("can't reach from 1")
-print(link(X,Y) & (~can_reach(1,X)) & (X !=1))
+	print("can't reach from 1")
+	print(link(X,Y) & (~can_reach(1,X)) & (X !=1))
 
-print("all path from/to 1")
-all_path(X,Y,P) <= all_path(X,Z,P2) & link(Z,Y) & (X!=Y) & (X._not_in(P2)) & (Y._not_in(P2)) & (P==P2+[Z])
-all_path(X,Y,P) <= link(X,Y) & (P==[])
+	print("all path from/to 1")
+	all_path(X,Y,P) <= all_path(X,Z,P2) & link(Z,Y) & (X!=Y) & (X._not_in(P2)) & (Y._not_in(P2)) & (P==P2+[Z])
+	all_path(X,Y,P) <= link(X,Y) & (P==[])
 
-print (all_path(X,1,P))
+	print (all_path(X,1,P))
 
-print("no path from 1")
-print(link(X,Y) & (~all_path(1,X,P)) & (X !=1))
+	print("no path from 1")
+	print(link(X,Y) & (~all_path(1,X,P)) & (X !=1))
 
-print("a path from / to 1")
-(path[X,Y]==P) <= ((path[X,Z]==P2) &  link(Z,Y)
-                   # dropping next line is an unsafe optimisation. see negation below
-                   #& (X!=Y) & (X._not_in(P2)) & (Y._not_in(P2)) 
-                   & (P==P2+[Z])) 
-(path[X,Y]==P) <= link(X,Y) & (P==[])
+	print("a path from / to 1")
+	(path[X,Y]==P) <= ((path[X,Z]==P2) &  link(Z,Y)
+					   # dropping next line is an unsafe optimisation. see negation below
+					   #& (X!=Y) & (X._not_in(P2)) & (Y._not_in(P2)) 
+					   & (P==P2+[Z])) 
+	(path[X,Y]==P) <= link(X,Y) & (P==[])
 
-print (path[1,Y]==P)
+	print (path[1,Y]==P)
 
-print("not one path from 1")
-(safe_path[X,Y]==P) <= ((safe_path[X,Z]==P2) &  link(Z,Y)
-                   # next line needed to avoid infinite loop in negation
-                   & (X!=Y) & (X._not_in(P2)) & (Y._not_in(P2)) 
-                   & (P==P2+[Z])) 
-(safe_path[X,Y]==P) <= link(X,Y) & (P==[])
-print(link(X,Y) & (X !=1) & (~(safe_path[X,1]==P)) )
+	print("not one path from 1")
+	(safe_path[X,Y]==P) <= ((safe_path[X,Z]==P2) &  link(Z,Y)
+					   # next line needed to avoid infinite loop in negation
+					   & (X!=Y) & (X._not_in(P2)) & (Y._not_in(P2)) 
+					   & (P==P2+[Z])) 
+	(safe_path[X,Y]==P) <= link(X,Y) & (P==[])
+	print(link(X,Y) & (X !=1) & (~(safe_path[X,1]==P)) )
 
-print ("path with cost from / to 1")
-(path_with_cost(X,Y,P,C)) <= (path_with_cost(X,Z,P2,C2)) & link(Z,Y) & (X!=Y) & (X._not_in(P2)) & (Y._not_in(P2)) & (P==P2+[Z]) & (C==C2+1) 
-(path_with_cost(X,Y,P,C)) <= link(X,Y) & (P==[]) & (C==0)
+	print ("path with cost from / to 1")
+	(path_with_cost(X,Y,P,C)) <= (path_with_cost(X,Z,P2,C2)) & link(Z,Y) & (X!=Y) & (X._not_in(P2)) & (Y._not_in(P2)) & (P==P2+[Z]) & (C==C2+1) 
+	(path_with_cost(X,Y,P,C)) <= link(X,Y) & (P==[]) & (C==0)
 
-print (path_with_cost(1,Y,P,C))
-print (path_with_cost(Y,1,P,C))
+	print (path_with_cost(1,Y,P,C))
+	print (path_with_cost(Y,1,P,C))
 
-print ("shortest path from / to 1")
-(shortest_path[X,Y]==min_(P, order_by=C)) <= (path_with_cost(X,Y,P,C))
+	print ("shortest path from / to 1")
+	(shortest_path[X,Y]==min_(P, order_by=C)) <= (path_with_cost(X,Y,P,C))
 
-print (shortest_path[1,Y]==P) 
-print (shortest_path[X,1]==P)
+	print (shortest_path[1,Y]==P) 
+	print (shortest_path[X,1]==P)
--- pyDatalog/examples/queens_N.py
+++ pyDatalog/examples/queens_N.py
@@ -2,33 +2,33 @@
 from pyDatalog import pyDatalog
 import time
 
-pyDatalog.create_terms('N,N1, X,Y, X0,X1,X2,X3,X4,X5,X6,X7')
-pyDatalog.create_terms('ok,queens, next_queen, pred, pred2')
+@pyDatalog.program()
+def _():
+	size=8
 
-size=8
+	# when is it ok to have a queen in row X1 and another in row X2, separated by N columns
+	# this is memoized !
+	queens(N, X)    <= (N>1) & queens(N-1, X[:-1]) & next_queen(N, X)
+	queens(1, X)    <= (X1._in(range(size))) & (X[0]==X1)
 
-# when is it ok to have a queen in row X1 and another in row X2, separated by N columns
-# this is memoized !
-queens(N, X)    <= (N>1) & queens(N-1, X[:-1]) & next_queen(N, X)
-queens(1, X)    <= (X1._in(range(size))) & (X[0]==X1)
+	next_queen(N, X) <= (N>2) & next_queen(N-1, X[1:]) & ok(X[0], N-1, X[-1]) 
+	next_queen(2, X) <= queens(1,(X1,)) & ok(X[0], 1, X1) & (X[1] == X1)
 
-next_queen(N, X) <= (N>2) & next_queen(N-1, X[1:]) & ok(X[0], N-1, X[-1]) 
-next_queen(2, X) <= queens(1,(X1,)) & ok(X[0], 1, X1) & (X[1] == X1)
+	ok(X1, N, X2) <= (X1 != X2) & (X1 != X2+N) & (X1 != X2-N)
 
-ok(X1, N, X2) <= (X1 != X2) & (X1 != X2+N) & (X1 != X2-N)
+	start_time = time.time()
+	print(queens(size, (X0,X1,X2,X3,X4,X5,X6,X7)))
+	print("First datalog run in %f seconds" % (time.time() - start_time))
 
-start_time = time.time()
-print(queens(size, (X0,X1,X2,X3,X4,X5,X6,X7)))
-print("First datalog run in %f seconds" % (time.time() - start_time))
-
-start = time.time()
-for i in range(20):
-    # there is a warm-up period for the JIT --> let's compute it again
-    start_time = time.time()
-    datalog_count = len(queens(size, (X0,X1,X2,X3,X4,X5,X6,X7)).data)
-    datalog_time = (time.time() - start_time)
-    print(datalog_time)
-print("Average : %s" % ((time.time() - start)/20))
+	start = time.time()
+	for i in range(20):
+		# there is a warm-up period for the JIT --> let's compute it again
+		start_time = time.time()
+		datalog_count = len(queens(size, (X0,X1,X2,X3,X4,X5,X6,X7)).data)
+		datalog_time = (time.time() - start_time)
+		print(datalog_time)
+	print("Average : %s" % ((time.time() - start)/20))
+	print("%i solutions by datalog in %f seconds" % (datalog_count, datalog_time))
 
 # pure python solution found on http://rosettacode.org/wiki/N-Queens#Python, for comparison purposes
 
@@ -47,6 +47,5 @@ start_time = time.time()
 queens() 
 python_time = time.time() - start_time
 
-print("%i solutions by datalog in %f seconds" % (datalog_count, datalog_time))
 print("python : %f seconds" % python_time)
 
